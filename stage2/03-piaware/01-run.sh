854e98f337d321d55515d5995a420776d6bd27cb
Eric Tran <eric1tran@gmail.com>
2018-06-18 18:39:33 +0000

    Fix config file IMG_NAME, switch ordering of package install, set +x on 01-run.sh

--- stage2/03-piaware/01-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-install -m 644 files/piaware-installer.list 		"${ROOTFS_DIR}/etc/apt/sources.list.d/"
-install -m 644 files/rsyslog.conf 			"${ROOTFS_DIR}/etc/"
-install -m 644 files/rpi-bootconfig			"${ROOTFS_DIR}/etc/default/"
-install -m 644 files/journald.conf			"${ROOTFS_DIR}/etc/systemd/"
-install -m 644 files/flightaware-archive-keyring/gpg	"${ROOTFS_DIR}/etc/apt/trusted.gpg.d/"
